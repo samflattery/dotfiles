7d00301ff2eb4353f032c4f7017ec789265f0b01
Sam Flattery <sflatter@andrew.cmu.edu>
2019-10-10 15:26:05 -0400

    Added aliases and ssh files

---
+++ aliases.sh
@@ -0,0 +1,71 @@
+# aliases
+function cdl {
+	builtin cd "$@" && ls -F
+}
+
+# easier navigation
+alias cdd="cdl .."
+alias cddd="cdl ../.."
+alias cdddd="cdl ../../.."
+alias cddddd="cdl ../../../.."
+
+# easily access files
+alias vimrc="vim ~/dotfiles/vimrc"
+alias zshrc="vim ~/dotfiles/zshrc"
+alias aliases="vim ~/dotfiles/aliases.sh"
+
+# verbose commands
+alias mv="mv -v"
+alias cp="cp -v"
+alias rm="rm -v"
+
+# misspellings
+alias cl="clear"
+alias clera="clear"
+alias clearl="clear"
+alias clears="clear"
+alias clearls="clear"
+alias sl="ls"
+alias l="ls"
+
+# git
+alias ga="git add ."
+alias gca="git commit -a"
+alias gcm="git commit -m"
+alias gcam="git commit -am"
+alias gs="git status"
+alias gd="git diff"
+
+# resuable format strings
+GIT_PRETTY_FORMAT="--pretty=\"%C(bold green)%h%Creset%C(auto)%d%Creset %s\""
+GIT_PRETTY_FORMAT_AUTHOR="--pretty=\"%C(bold green)%h%Creset %C(yellow)%an%Creset%C(auto)%d%Creset %s\""
+
+# pretty Git log
+alias gl="git log --graph $GIT_PRETTY_FORMAT"
+# pretty Git log, all references
+alias gll='gl --all'
+# pretty Git log, show authors
+alias gla="git log --graph $GIT_PRETTY_FORMAT_AUTHOR"
+# pretty Git log, all references, show authors
+alias glla='gla --all'
+
+# ssh
+alias andrew="ssh andrew"
+alias shark="ssh shark"
+alias buggy="ssh buggy"
+
+# open programs
+alias ubuntu="open -a VMware\ Fusion"
+alias ch="open -a Google\ Chrome"
+alias prev="open -a Preview"
+alias finder="open -a Finder"
+alias twothirteen="open https://www.cs.cmu.edu/~213/schedule.html"
+
+# random stuff
+alias wttr="curl 'wttr.in?format=3'"
+alias tmux="tmux -2"
+alias sml="rlwrap sml"
+alias pip="pip3"
+alias duls="du -h -d1 | sort -hr"
+
+
